3a1159089cfbd40e45b421d88f9aa0d87067487c
June Hosoya <junehosoya@gmail.com>
2019-02-22 14:18:18 -0500

    Updated Thunk creator

--- client/store/users.js
+++ client/store/users.js
@@ -8,7 +8,7 @@ const initialState = {
 
 //Action
 const GET_ALL_USERS = "GET_ALL_USERS";
-const GET_SINGLE_USER = "GET_SINGLE_USER";
+const GET_USER = "GET_USER";
 
 //Action creator
 const gotAllUsers = users => ({
@@ -16,17 +16,35 @@ const gotAllUsers = users => ({
   users
 });
 
-const gotSingleUser = user => ({
-  type: GET_SINGLE_USER,
+const gotMe = user => ({
+  type: GET_USER,
   user
 });
+
+//---------------THUNK CREATOR--------------
+
+//LOGIN
+export const login = formData => {
+  return async dispatch => {
+    const { data: user } = await axios.put("/auth/login", formData);
+    dispatch(gotMe(user));
+  };
+};
+
+//USER'S HOMEPAGE
+export const getMe = () => {
+  return async dispatch => {
+    const { data: user } = await axios.get("/auth/me");
+    dispatch(gotMe(user));
+  };
+};
 
 //Reducer
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case "GET_ALL_USERS":
       return { ...state, users: [...state.users, action.users] };
-    case "GET_SiNGLE_USER":
+    case "GET_USER":
       return { ...state, user: action.user };
   }
   return state;
