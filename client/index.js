5d3a870b80de8e3df1f6c27fb4e5e52ff711fcad
June Hosoya <junehosoya@gmail.com>
2019-02-22 14:25:25 -0500

    Added Router in the index.js

--- client/index.js
+++ client/index.js
@@ -2,14 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter
+} from "react-router-dom";
+
 import Root from "./components/Root";
 import "../public/src/main.css";
 
 ReactDOM.render(
-  <div>
-    <Provider store={store}>
+  <Provider store={store}>
+    <Router>
       <Root />
-    </Provider>
-  </div>,
+    </Router>
+  </Provider>,
   document.getElementById("app")
 );
