0518cee73675ee2935dba4464cf40a6fa22c4c8b
June Hosoya <junehosoya@gmail.com>
2019-02-22 14:38:49 -0500

    done with root getme

--- client/components/Root.js
+++ client/components/Root.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import UserPage from "./UserPage";
-const Root = () => {
-  return (
-    <div>
-      <UserPage />
-    </div>
-  );
-};
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter
+} from "react-router-dom";
+import { getMe } from "../store/users";
+// const Root = () => {
+//   return (
+//     <div>
+//       <UserPage />
+//     </div>
+//   );
+// };
 
-export default Root;
+class Root extends Component {
+  constructor(props) {
+    super(props);
+  }
+  async componentDidMount() {
+    await store.dispatch(getMe());
+    this.props.history.push("/home");
+  }
+  render() {
+    return (
+      <div>
+        <UserPage />
+      </div>
+    );
+  }
+}
+
+export default withRouter(Root);
--- client/index.js
+++ client/index.js
@@ -2,12 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  withRouter
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import Root from "./components/Root";
 import "../public/src/main.css";
